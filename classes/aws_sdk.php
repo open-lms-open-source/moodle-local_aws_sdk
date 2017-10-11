964569e7e65e0e2ec606d4c457808a660422a5d6
Mark Nielsen <mark@moodlerooms.com>
2017-10-11 15:52:00 -0700

    INT-12420: Allow the key/secret to be optional for S3 file system

--- classes/aws_sdk.php
+++ classes/aws_sdk.php
@@ -37,7 +37,7 @@ class aws_sdk {
     /**
      * Required AWS config names.
      */
-    const REQUIRED_CONFIGS = ['key', 'secret', 'region'];
+    const REQUIRED_CONFIGS = ['region'];
 
     /**
      * Autoload the AWS SDK classes.
@@ -69,12 +69,51 @@ class aws_sdk {
             }
         }
 
-        return [
-            'region'      => $cfg['region'],
-            'credentials' => [
+        $config = ['region' => $cfg['region']];
+
+        $credentials = self::resolve_credentials($name, $cfg);
+        if (!empty($credentials)) {
+            $config['credentials'] = $credentials;
+        }
+
+        return $config;
+    }
+
+    /**
+     * Extract AWS credentials from CFG value.
+     *
+     * @param string $name The CFG name.
+     * @param array $cfg The Moodle config value.
+     * @return mixed
+     */
+    private static function resolve_credentials($name, array $cfg) {
+        // First check for key/secret credentials.
+        if (array_key_exists('key', $cfg) && array_key_exists('secret', $cfg)) {
+            return [
                 'key'    => $cfg['key'],
                 'secret' => $cfg['secret']
-            ],
-        ];
+            ];
+        }
+
+        // With no credentials set, then the AWS SDK will fallback to various means for acquiring credentials.
+        // See http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/configuration.html#credentials for details.
+
+        // When using IAM roles, we can set credentials to a cache so the SDK doesn't have to ping the metadata service every time.
+        // See http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/credentials.html#instance-profile-credentials for details.
+        if (array_key_exists('credentials_cache', $cfg)) {
+            // Ensure we are loading as caches require this.
+            self::autoload();
+
+            $type = $cfg['credentials_cache'];
+            if ($type === 'apcu') {
+                if (aws_apcu_cache::are_requirements_met()) {
+                    return new aws_apcu_cache();
+                }
+            } else {
+                throw new \coding_exception('The $CFG->'.$name.'[\'credentials_cache\'] value is unknown: '.$type);
+            }
+        }
+
+        return [];
     }
 }
